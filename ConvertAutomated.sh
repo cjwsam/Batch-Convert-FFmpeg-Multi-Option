a81cb355db99606daf95bbe840b20322b5d29e18
cjwsam <cjwsam@gmail.com>
2019-11-25 15:00:11 +0100

    Add files via upload

--- ConvertAutomated.sh
+++ ConvertAutomated.sh
@@ -15,22 +15,16 @@ filename="${SOURCE_FILE%.*}"
 vcodex=$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "${SOURCE_FILE}")
 acodex=$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "${SOURCE_FILE}")
 scodex=$(ffprobe -v error -select_streams s:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "${SOURCE_FILE}")
-logFname="TVlog.txt"
-condencedCheck="[DONE]-$fName"
-while IFS= read -r line; do
-if [[ $line == $condencedCheck ]] ; then
-        uamaua="yes"
-fi
-done < "$logFname"
 
 
-if [[ ($vcodex == "h264") && ($acodex == "aac") && ($scodex == "") || ($uauma == "yes") ]]; then
+if [[ ($vcodex == "h264") && ($acodex == "aac") && ($scodex == "") ]]; then
 
  echo ""   
  echo "--------------------------------------------------------"   
  echo "Since this file is Done It Will be Skipped "
  echo "--------------------------------------------------------"
  echo ""   
+
  exit 0
 
 elif [[ ($vcodex == "h264") && ($acodex == "aac") && ($scodex != "") ]]; then
@@ -41,6 +35,8 @@ elif [[ ($vcodex == "h264") && ($acodex == "aac") && ($scodex != "") ]]; then
     echo "File Containes SUBS they Will be stripped "
     echo "--------------------------------------------------------"
     echo ""   
+
+    ffmpeg -hide_banner -i "$SOURCE_FILE" -y  "${filename}.en.srt"
     ffmpeg -hide_banner -i "$SOURCE_FILE" -c copy -ac 2 -movflags +faststart -sn -f mp4 -y "${filename}-Converting.mp4"
 exitCode=$?
 
@@ -55,7 +51,7 @@ exitCode=$?
 				fi
 
         fi
-echo "[DONE]-$fName" >> /nas/CONVERT/TVlog.txt
+
         exit 0
     else
         exit $exitCode
@@ -71,6 +67,8 @@ echo "--------------------------------------------------------"
 echo "File Video is h264 but the AUDIO will be encoded to AAC"
     echo "--------------------------------------------------------" 
 echo ""   
+
+ffmpeg -hide_banner -i "$SOURCE_FILE" -y  "${filename}.en.srt"
 ffmpeg -hide_banner -i "$SOURCE_FILE" -c:v copy -c:a libfdk_aac -ac 2 -movflags +faststart -sn -f mp4 -y "${filename}-Converting.mp4"
     exitCode=$?
 
@@ -85,14 +83,13 @@ ffmpeg -hide_banner -i "$SOURCE_FILE" -c:v copy -c:a libfdk_aac -ac 2 -movflags 
 				fi
 
         fi
-echo "[DONE]-$fName" >> /nas/CONVERT/TVlog.txt
         exit 0
     else
         exit $exitCode
     fi
 
 
-elif [[ ($vcodex != "h264") ]]; then
+elif [[ ($vcodex != "h264") && ($vcodex != "hevc")]]; then
 
 echo ""   
     echo "----------------------------------------------------"    
@@ -102,7 +99,8 @@ echo ""
 
 #Change this to ur full encoding prefs
 
-ffmpeg -hide_banner -i "$SOURCE_FILE" -pix_fmt yuv420p -deinterlace -bsf:v "filter_units=remove_types=6" -c:v libx264 -bf 2 -coder 1 -threads 0 -preset medium -profile:v high -g 15 -tune film -acodec libfdk_aac  -ac 2 -movflags +faststart -sn -f mp4 -y "${filename}-Converting.mp4"
+ffmpeg -hide_banner -i "$SOURCE_FILE" -y  "${filename}.en.srt"
+ffmpeg -hide_banner -i "$SOURCE_FILE" -c:v libx264 -threads 6 -tune film -acodec libfdk_aac -ac 2 -movflags +faststart -sn -f mp4 -y "${filename}-Converting.mp4"
  
     exitCode=$?
 
@@ -117,11 +115,12 @@ ffmpeg -hide_banner -i "$SOURCE_FILE" -pix_fmt yuv420p -deinterlace -bsf:v "filt
 				fi
 
         fi
-echo "[DONE]-$fName" >> /nas/CONVERT/TVlog.txt
+
         exit 0
     else
         exit $exitCode
     fi
 
 fi
+
 
